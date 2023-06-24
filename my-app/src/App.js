b1475d8aeeb261349be81e508340be0cc84d11d1
Vinícius Rampelotti <viniciusrampeloti@gmail.com>
2023-06-24 07:01:05 -0300

    Criação do código

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  const name = 'Vinicius';
+  const message = 'Olá mundo!';
+
+  return (
+    <div>
+      <h1>Olá, {name}!</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
